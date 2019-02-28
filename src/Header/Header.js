b45d67f47d3bef445ef89d86ccd67043c484427d
Yanitsa <yani.valeva@gmail.com>
2019-03-01 01:44:31 +0200

    Replace reactstrap with material-ui

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Button } from 'reactstrap';
+import Button from '@material-ui/core/Button';
+import green from '@material-ui/core/colors/green';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const themeStyles = theme => ({
+    cssRoot: {
+        color: 'rgb(255, 255, 255)'
+    }
+});
+
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+    }
+});
+
 
 const header = props =>
     (<header>
         <div className={styles.HeadContainer}>
-            <h3 className={styles.HeadH3}>facebook</h3>
-            <Button color="success">Sign up</Button>
+            <h2 className={styles.HeadH3}>facebook</h2>
+            <MuiThemeProvider theme={theme}>
+                <Button variant="contained" color="primary" className={props.classes.cssRoot}>Sign up</Button>
+            </MuiThemeProvider>
         </div>
     </header>);
 
-export default header;
+export default withStyles(themeStyles)(header);
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
-import { Button, Input } from 'reactstrap';
+import Button from '@material-ui/core/Button';
 import styles from './Main.module.css';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core';
+
+const themeStyles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+    },
+    dense: {
+      marginTop: 16,
+    },
+    menu: {
+      width: 200,
+    },
+  });
 
 class Main extends Component {
     render() {
+        const { classes } = this.props;
+
         return (
             <div className={styles.LoginContainer}>
                 <form>
-                    <span>Log into Facebook</span>
-                    <Input placeholder="Email" type="text" />
-                    <Input placeholder="Password" type="password" />
+                    <h2>Log into Facebook</h2>
+                    <TextField
+                        id="outlined-email-input"
+                        label="Email"
+                        className={classes.textField}
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-password-input"
+                        label="Password"
+                        className={classes.textField}
+                        type="password"
+                        autoComplete="current-password"
+                        margin="normal"
+                        variant="outlined"
+                    />
                     <Button color="primary">Log in</Button>
                     <span>or</span>
                     <Button color="success">Create New Account</Button>
@@ -19,5 +57,5 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default withStyles(themeStyles)(Main);
 
