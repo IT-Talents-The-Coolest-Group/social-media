b17960647922b8d41cf100035bfaf04d904ec65c
Yanitsa <yani.valeva@gmail.com>
2019-03-01 23:19:50 +0200

    Add 404 handler

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import HomeHeader from '../HomeHeader/HomeHeader';
 import UserProfile from '../UserProfile/UserProfile';
 import UserProfileHome from '../UserProfileHome/UserProfileHome';
 import LoginContainer from '../Login/LoginContainer';
+import Link from '../UI/Link/Link';
 
 class App extends Component {
   render() {
@@ -12,9 +13,14 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Switch>
-            <Route exact path="/profile" render={() => {return (<><HomeHeader /> <UserProfile/></>) }} />
-            <Route exact path="/profile-home" render={() => {return (<><HomeHeader /> <UserProfileHome/></>) }} />
-            <Route exact path="/" render={() => {return (<LoginContainer />) }} />
+            <Route exact path="/profile" render={() => { return (<><HomeHeader /> <UserProfile /></>) }} />
+            <Route exact path="/profile-home" render={() => { return (<><HomeHeader /> <UserProfileHome /></>) }} />
+            <Route exact path="/" render={() => { return (<LoginContainer />) }} />
+            
+            {/* NOT FOUND page */}
+            <Route render={
+              () => <><h1>404 Page not found!</h1><Link to="/">Home</Link></>
+            } />
           </Switch>
         </div>
       </BrowserRouter>
