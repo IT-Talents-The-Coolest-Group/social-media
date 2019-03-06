3b4862e6052ee1ff708b6152d7d0b1406091e4a2
Yanitsa <yani.valeva@gmail.com>
2019-03-06 12:55:39 +0200

    Deleted url port

--- src/Register/RegisterContent.js
+++ src/Register/RegisterContent.js
@@ -115,7 +115,7 @@ class RegisterContent extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         const hasErrors = this.validate();
-        let url = 'http://bacefookapi.herokuapp.com:8090/signup';
+        let url = 'http://bacefookapi.herokuapp.com/signup';
         if (!hasErrors) {
             const data = {
                 firstName: this.state.firstName,
